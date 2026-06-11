0ed62f1e4e2f267b8f0e8c120e98f90afd886f18
Nuris fps <abzalnurislam8@gmail.com>
2026-06-11 15:24:59 +0500

    update

---
+++ Practice1/python-basics/comments.py
@@ -0,0 +1,7 @@
+#This is a comment in Python.
+print("Hello, Python!") #this is also a comment
+"""
+print("multi comment, it is not executed")
+1
+2
+"""
---
+++ Practice1/python-basics/conversion.py
@@ -0,0 +1,9 @@
+x = 5.5
+ix = int(x)
+y = 5
+fy = float(y)
+z = 5j
+cz = complex(y)
+print(ix)
+print(fy)    
+print(cz)
